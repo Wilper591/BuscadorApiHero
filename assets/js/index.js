f14a307d3bfb2472ac4d443d3dbf599726cba181
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-02-22 21:20:56 -0300

    Corrigiendo bug en inputHero, permitia el ingreso de letras y dejaba de funcionar, se añade regex

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,16 @@
 $("#btnInput").on("click", function () {
   limpiarHTMLError();
   let heroId = Number($("#inputHero").val());
-  if (heroId <= 0 || heroId > 731 || heroId === "") {
+  let validadorHero = /^(?:[1-9]|[1-9][0-9]|[1-9][0-9][0-9]|7[0-2][0-9]|73[0-1])$/;
+  if (validadorHero.test(heroId)) {
+    $("#resultadoHero").ApiHero();
+  } else {
     let errormsj = $("#errorHero").append(
       `<p class="text-center fs-2">Debes ingresar un numero entre 1 y 731</p>`
     );
     setTimeout(() => {
       errormsj.empty();
     }, 5000);
-  } else {
-    $("#resultadoHero").ApiHero();
   }
 });
 
@@ -17,4 +18,3 @@ function limpiarHTMLError() {
   const errorHero = $("#errorHero");
   errorHero.empty();
 }
-
